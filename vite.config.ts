053863eb7b0f55005cf6c15b34c9cf9b74060f8a
Md. Rakib Trofder <mdrakibtrofder@gmail.com>
2026-04-16 19:33:39 +0600

    feature: configuration for deploying at github

--- vite.config.ts
+++ vite.config.ts
@@ -13,6 +13,7 @@ export default defineConfig(({ mode }) => ({
     },
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+    base: "/sustain-stack/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
